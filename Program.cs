7cc7f561cae087e107025ba42eb33d58c5f9497d
Kelly L Dobson <kellyfinolmt@gmail.com>
2021-08-16 20:56:58 -0400

    Ready to submit homework

--- Program.cs
+++ Program.cs
@@ -2,11 +2,48 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            int numberOfCupsOfCoffee = 6;
+
+            string fullName = "Kelly Dobson";
+
+            DateTime today = System.DateTime.Now;
+
+            System.Console.WriteLine(numberOfCupsOfCoffee + " " + fullName + " " + today);
+
+            Console.Write("Enter your username: ");
+
+            string userName = Console.ReadLine();
+
+            if (userName == "Alice")
+            {
+                Console.WriteLine("The world keeps shifting so quickly under your feet!");
+            }
+
+            Console.WriteLine("Hello, " + userName + "!");
+            Console.Write("Enter a number: ");
+            string firstNumberAsString = Console.ReadLine();
+            Console.Write("Enter Another Number: ");
+            string secondNumberAsString = Console.ReadLine();
+
+            double firstOperand = double.Parse(firstNumberAsString);
+            double secondOperand = double.Parse(secondNumberAsString);
+
+            double sum = firstOperand + secondOperand;
+            double difference = firstOperand - secondOperand;
+            double product = firstOperand * secondOperand;
+            double quotient = firstOperand / secondOperand;
+            double remainder = firstOperand % secondOperand;
+
+            Console.WriteLine("sum: " + sum);
+            Console.WriteLine("difference: " + difference);
+            Console.WriteLine("product: " + product);
+            Console.WriteLine("quotient: " + quotient);
+            Console.WriteLine("remainder: " + remainder);
+
+        }
     }
-  }
 }
